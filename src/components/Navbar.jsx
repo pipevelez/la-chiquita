99739a1da32074e5792fac2ed0d67529913e5692
Pipe Velez <pipevv999@gmail.com>
2025-07-02 00:45:39 -0500

    Se prueba nueva fuenta para LA CHIQUITA OK

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
             height="60"
             className="me-2 rounded-circle"
           />
-          La Chiquita
+          LA CHIQUITA
         </Link>
 
         <button
